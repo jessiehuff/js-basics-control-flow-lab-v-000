3d33a810d5d1b01afc8b18f1f1f1914e4d7711ea
Jessie Huff <huffjd1@gmail.com>
2018-02-19 16:19:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function scuberGreetingForFeet(feet) {
   if(feet <= 400) {
-    return "This one is on me!"
+    return "This one is on me!";
   }
   elseif (feet > 2500) {
-    return "No can do."
+    return "No can do.";
   }
   elseif (feet > 2000){
-    return "I will gladly take your thirty bucks."
+    return "I will gladly take your thirty bucks.";
+  }
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default: 
+      return "Bye."; 
   }
 }
