5795ed9f4fb609abec3c569d1e1952050617e6f7
Jessie Huff <huffjd1@gmail.com>
2018-02-19 17:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function scuberGreetingForFeet(feet) {
-  if (feet <= 400); {
-    return "This one is on me!";
-  }  elseif (feet > 2500); {
-    return "No can do.";
-  }  elseif (feet > 2000);{
-    return "I will gladly take your thirty bucks.";
+  if (feet <= 400) {
+    return "This one is on me!"
+  }  elseif (feet > 2500) {
+    return "No can do."
+  }  elseif (feet > 2000){
+    return "I will gladly take your thirty bucks."
   }
 }
 
