7b9ad940ac6037c260fe36ba641f90ace6fb2079
Jessie Huff <huffjd1@gmail.com>
2018-02-19 17:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ function ternaryCheckCity(city){
   return (city === "NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(tip){
-  switch (tip) {
-    case "generous":
-      return "Thank you so much."
-      break;
-    case "not as generous":
-      return "Thank you."
-      break;
-    default:
-      return "Bye."
-      break;
-  }
-}
+// function switchOnCharmFromTip(tip){
+//   switch (tip) {
+//     case "generous":
+//       return "Thank you so much."
+//       break;
+//     case "not as generous":
+//       return "Thank you."
+//       break;
+//     default:
+//       return "Bye."
+//       break;
+//   }
+// }
