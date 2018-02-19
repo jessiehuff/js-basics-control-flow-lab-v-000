da6ab250c6112c8179d770742dd10abf594e74f8
Jessie Huff <huffjd1@gmail.com>
2018-02-19 17:21:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ function scuberGreetingForFeet(feet) {
     return "I will gladly take your thirty bucks."
   }
 }
- function ternaryCheckCity(city){   return (city === "NYC" ? "Ok, sounds good." : "No go.") }
+
+ function ternaryCheckCity(city){
+   return (city === "NYC" ? "Ok, sounds good." : "No go.")
+ }
 
 // function switchOnCharmFromTip(tip){
 //   switch (tip) {
