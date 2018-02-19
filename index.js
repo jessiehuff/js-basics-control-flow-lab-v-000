b8b27899dc0e5fc97a207d31a4ccc72a3196f462
Jessie Huff <huffjd1@gmail.com>
2018-02-19 17:07:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function scuberGreetingForFeet(feet) {
-//  return "This one is on me!";
-//  }  elseif (feet > 2500) {
-//    return "No can do.";
-//  }  elseif (feet > 2000){
-//    return "I will gladly take your thirty bucks.";
-//  }
+  if (feet <= 400) {
+    return "This one is on me!"
+  }  elseif (feet > 2500); {
+    return "No can do."
+  }  elseif (feet > 2000);{
+    return "I will gladly take your thirty bucks."
+  }
 }
 
 function ternaryCheckCity(city){
-//  return (city === "NYC" ? "Ok, sounds good." : "No go.";)
+  return (city === "NYC" ? "Ok, sounds good." : "No go.";)
 }
 
 function switchOnCharmFromTip(tip){
