9b43b6b76ab04a3dda94cafcc402fb1ebad74bce
Jessie Huff <huffjd1@gmail.com>
2018-02-19 16:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ function ternaryCheckCity(city){
 }
 
 function switchOnCharmFromTip(tip){
-  switch (tip) {
-    case "generous":
-      return "Thank you so much."
-      break;
-    case "not as generous":
-      return "Thank you."
-      break;
-    default:
-      return "Bye."
-      break;
+//  switch (tip) {
+//    case "generous":
+//      return "Thank you so much."
+//      break;
+//    case "not as generous":
+//      return "Thank you."
+//      break;
+//    default:
+//      return "Bye."
+//      break;
   }
 }
