4153aa2a38f113d8297a5ecda5d5a5acc7170eba
Jessie Huff <huffjd1@gmail.com>
2018-02-19 16:11:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
-    return "This one is on me!";
+    return "This one is on me!"
   }
   elseif (feet > 2000 && feet <= 2500){
-    return "I will gladly take your thirty bucks.";
+    return "I will gladly take your thirty bucks."
   }
   elseif (feet > 2500) {
-    return "No can do.";
+    return "No can do."
   }
 }
